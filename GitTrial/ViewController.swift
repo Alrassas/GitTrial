facac28ca12bdb7f9435f256313a92e70476e713
Mohammed Alrassas <Mohammed Alrassas>
2020-04-29 15:22:03 +0200

    reverse changes to be like prev

--- GitTrial/ViewController.swift
+++ GitTrial/ViewController.swift
@@ -13,12 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //hello
     }
-    func didYouDid()
-    {
-        
-    }
+
 
 }
 
